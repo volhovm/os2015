5af41ee43658f86be666d3e9cc55ad3b3fc6282b
volhovM <volhovm.cs@gmail.com>
2015-03-13 03:30:18 +0300

    scratch added

---
+++ lib/helpers.c
@@ -0,0 +1,10 @@
+#include "helpers.h"
+#include <sys/types.h>
+
+ssize_t read_(int fd, void *buf, size_t count) {
+    return -1;
+}
+
+ssize_t write_(int fd, const void *buf, size_t count) {
+    return -1;
+}
---
+++ lib/helpers.h
@@ -0,0 +1,8 @@
+#ifndef HELPERS_C
+#define HELPERS_C
+#include <sys/types.h>
+
+ssize_t read_(int fd, void *buf, size_t count);
+ssize_t write_(int fd, const void *buf, size_t count);
+
+#endif
