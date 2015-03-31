e0949595c857256ce89ac3102a8b236787b10ef0
volhovM <volhovm.cs@gmail.com>
2015-04-01 01:08:10 +0300

    basic bufer functions added

--- lib/bufio.c
+++ lib/bufio.c
@@ -1,7 +1,49 @@
-#include "bufio.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <bufio.h>
 
-struct buf_t *buf_new(size_t capacity);
-void buf_free(struct buf_t *);
-size_t buf_capacity(buf_t *);
-size_t buf_size(buf_t *);
-ssize_t buf_fill(fd_t fd, buf_t *buf, size_t required);
+void chnl(struct buf_t* buf) {
+#ifdef DEBUG
+    if (buf == NULL) {
+        abort();
+    }
+#endif
+}
+
+struct buf_t* buf_new(size_t capacity) {
+    void* data = malloc(capacity);
+    if (data == NULL) return NULL;
+    void* space = malloc(sizeof(struct buf_t));
+    if (space == NULL) return NULL;
+    struct buf_t* buf = (struct buf_t*) space;
+    buf->data = data;
+    buf->capacity = capacity;
+    buf->size = 0;
+    return buf;
+}
+
+void buf_free(struct buf_t* buf) {
+    chnl(buf);
+    free(buf->data);
+    free(buf);
+}
+
+size_t buf_capacity(struct buf_t* buf) {
+    chnl(buf);
+    return buf->capacity;
+}
+
+size_t buf_size(struct buf_t* buf) {
+    chnl(buf);
+    return buf->size;
+}
+
+ssize_t buf_fill(fd_t fd, struct buf_t* buf, size_t required) {
+    chnl(buf);
+    return 0;
+}
+
+ssize_t buf_flush(fd_t fd, struct buf_t *buf, size_t required) {
+    chnl(buf);
+    return -1;
+}
